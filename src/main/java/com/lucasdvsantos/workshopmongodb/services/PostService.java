8ffa0dc4e62e9b7fad71e99bc16cae48bfaf45b0
Lucas David <santoslucasdavid@gmail.com>
2024-11-21 11:07:22 -0300

    Operation to find post by id added

---
+++ src/main/java/com/lucasdvsantos/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.lucasdvsantos.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasdvsantos.workshopmongodb.domain.Post;
+import com.lucasdvsantos.workshopmongodb.repository.PostRepository;
+import com.lucasdvsantos.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
