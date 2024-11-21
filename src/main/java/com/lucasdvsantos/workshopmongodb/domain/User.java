661dfcfecfd269c14acd57268779ca78658c57c6
Lucas David <santoslucasdavid@gmail.com>
2024-11-21 10:42:56 -0300

    Referencing user posts

--- src/main/java/com/lucasdvsantos/workshopmongodb/domain/User.java
+++ src/main/java/com/lucasdvsantos/workshopmongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.lucasdvsantos.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user") // You can also just use @Document, the collection will be mapped automatically based on the name of the class in lower case
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -46,6 +52,14 @@ public class User implements Serializable{
 	
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
+	public void addPost(Post post) {
+		posts.add(post);
 	}
 
 	@Override
