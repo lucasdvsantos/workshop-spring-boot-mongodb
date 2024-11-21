e166c2365c8cadc8b9f3f7e030d6fc86bdbf27eb
Lucas David <santoslucasdavid@gmail.com>
2024-11-21 10:57:44 -0300

    Endpoint to return a user's posts added

--- src/main/java/com/lucasdvsantos/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/lucasdvsantos/workshopmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucasdvsantos.workshopmongodb.domain.Post;
 import com.lucasdvsantos.workshopmongodb.domain.User;
 import com.lucasdvsantos.workshopmongodb.dto.UserDTO;
 import com.lucasdvsantos.workshopmongodb.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 			obj.setId(id);
 			obj = service.update(obj);
 			return ResponseEntity.noContent().build();
+		}
+		
+		@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+		public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+			User obj = service.findById(id);
+			return ResponseEntity.ok().body(obj.getPosts());
 		}
 
 }
